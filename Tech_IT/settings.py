b35fab2cb8a2c6ec8b8f0f138c774278c4333739
Nahid Islam <148832754+NahidIslamN@users.noreply.github.com>
2024-07-02 00:22:08 +0600

    Update settings.py

--- Tech_IT/settings.py
+++ Tech_IT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b$!$(_kg3ofk$-bh^v+z63=1%a974vfxctimt3eb3@g*h)fa8+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.versel.app']
 
 
 # Application definition
